b222e7c6bfb1c44a88ce35d593ade866cc3908fd
Chad Basinger <chad.basingeris@gmail.com>
2021-04-08 22:27:59 -0600

    working on routing now. also itemlist.js item list not mapping

--- src/App.js
+++ src/App.js
@@ -3,14 +3,36 @@ import UploadImage from './components/AWS-SDK/UploadImage'
 import { Component } from 'react';
 import Header from './components/Header/Header'
 import ItemList from './components/ItemList/ItemList'
+import axios from 'axios'
 
 class App extends Component {
-  constructor(){
-    super()
+  constructor(props){
+    super(props)
       this.state = {
-        imageURL: ''
+        // imageURL: '',
+        items: {}
       }
+      this.getAllItems = this.getAllItems.bind(this)
 
+  }
+
+  componentDidMount(){
+    this.getAllItems()
+    console.log(this.state.items)
+  }
+
+  getAllItems() {
+    // axios GET to /api/item/all here
+    axios.get('/api/item/all')
+    .then(responseItems => {
+        console.log('response items', responseItems)
+      this.setState({
+        items: responseItems
+        
+      })
+      console.log('state items', this.state.items)
+    })
+    .catch(err => console.log(err.response))
   }
 
 
@@ -24,8 +46,8 @@ class App extends Component {
           Search Input here, Filter By:
         </div>
         <section className="main-section">
-          <div>
-            <ItemList />
+          <div className="item-list">
+            <ItemList itemList={this.state.items}/>
           </div>
         </section>
         {/* <div>
--- src/components/ItemList/ItemList.js
+++ src/components/ItemList/ItemList.js
@@ -3,44 +3,27 @@ const { Component } = require("react");
 
 
 class ItemList extends Component {
-    constructor(){
-        super()
-        this.state = {
-            items: {}
-        }
-
-        this.getAllItems = this.getAllItems.bind(this)
+    constructor(props){
+        super(props)
+        
     }
 
-    componentDidUpdate(){
-        this.getAllItems()
-    }
-
-    getAllItems() {
-        // axios GET to /api/item/all here
-        axios.get('api/item/all')
-        .then(responseItems => {
-            console.log(responseItems)
-          this.setState({
-            items: responseItems
-            
-          })
-        })
-        .catch(err => console.log(err.response.request.response))
-      }
+    
 
     render(){
-        console.log(this.state.items)
+        console.log('props itemList.data', this.props.itemList.data)
         return (
-            <div>
-                {/* {this.state.items.map((element, index) => {
+            //when I have the map uncommented, the props doesn't load correctly. Need to ask for help from instructors.
+            <div className="items">
+                
+                {/* {this.props.itemList.data.map((element, index) => {
                     return (
                     <div className='individual-item' key={index}>
                         <img src={element.main_img_url} alt={element.item_name}
                          onClick={() => window.open(element.main_img_url)}/>
 
-                        <span className='hat-title-text'>{element.item_name}</span>
-                        <span className='hat-price'>{element.item_price}</span>
+                        <span className='item-title-text'>{element.item_name}</span>
+                        <span className='item-price'>{element.item_price}</span>
                     </div>
                 )}) 
                 } */}
